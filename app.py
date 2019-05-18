a6132dfa06688b0a5721cb5e4f4c2e89fac745ec
BaconRemovalUnit <suizsq@qq.com>
2019-05-18 14:07:36 -0700

    Check boxes

--- app.py
+++ app.py
@@ -10,10 +10,21 @@ def index():
     elif request.method == "POST":  # after user form submission, handle form submission here
         invest_form = request.form.to_dict()
         input_dollar = float(invest_form['input_dollar'])
-        strat = invest_form['strat']
-        print("user spending ${} with strat {}".format(input_dollar, strat))
+        # print("user spending ${} with strat {}".format(input_dollar, strat))
+        strat = []
+        if request.form.get("e"):
+            strat.append("e")
+        if request.form.get("g"):
+            strat.append("g")
+        if request.form.get("i"):
+            strat.append("i")
+        if request.form.get("q"):
+            strat.append("q")
+        if request.form.get("v"):
+            strat.append("v")
 
         # TODO add return info about stock here
+
 
         # things to return to the html, it can be any type
         obj = {"money":input_dollar, "strat": strat}
