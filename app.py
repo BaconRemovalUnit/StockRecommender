4f66ae1de59677877832a785dac4ead6c8366261
Amir Radman <amirradman@gmail.com>
2019-05-18 14:53:24 -0700

    stock association checkbox

--- app.py
+++ app.py
@@ -11,30 +11,34 @@ def index():
         invest_form = request.form.to_dict()
         input_dollar = float(invest_form['input_dollar'])
         # print("user spending ${} with strat {}".format(input_dollar, strat))
-        strat = []
-        if request.form.get("e"):
-            strat.append("e")
-        if request.form.get("g"):
-            strat.append("g")
-        if request.form.get("i"):
-            strat.append("i")
-        if request.form.get("q"):
-            strat.append("q")
-        if request.form.get("v"):
-            strat.append("v")
-
-        # TODO add return info about stock here
         stocksMapping = {
         "e" : ["APPL","ADBE","NSRGY"],
         "g" : ["NFLX","FB","NVDA"], 
         "i" : ["VTI","IXUS","ILTB"],
         "q" : ["LOW","ROST","TJX"],
-        "v" : ["WMT","BAC","CLF"],
-        };
+        "v" : ["WMT","BAC","CLF"]
+        }
+        strat = []
+        if request.form.get("e"):
+            strat.append(stocksMapping["e"])
+        if request.form.get("g"):
+            strat.append(stocksMapping["g"])
+        if request.form.get("i"):
+            strat.append(stocksMapping["i"])
+        if request.form.get("q"):
+            strat.append(stocksMapping["q"])
+        if request.form.get("v"):
+            strat.append(stocksMapping["v"])
+
+        # TODO add return info about stock here
 
 
         # things to return to the html, it can be any type
-        obj = {"money":input_dollar, "strat": strat}
+        for i in strat:
+            obj = {
+            "money":input_dollar, 
+            "strat": strat
+            }
 
         # render index with obj
         return render_template("index.html", obj=obj)
